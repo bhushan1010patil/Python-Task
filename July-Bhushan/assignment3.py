6b380991b53765e67a0dd4e0626c4c4e8f970b4d
bhushan patil <68237531+bhushan1010patil@users.noreply.github.com>
2020-07-18 16:36:55 +0530

    Add files via upload

---
+++ July-Bhushan/assignment3.py
@@ -0,0 +1,10 @@
+username="bhushan@1234"
+password=1234
+
+uname=input("Enter user name :")
+password1=int(input("Enter password :"))
+
+if (uname==username and password1==password):
+    print(f"Hello {uname} ,Welcome!")
+else:
+    print("Please enter valid Username")
